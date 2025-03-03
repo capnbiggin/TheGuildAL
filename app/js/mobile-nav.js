a47097a2cb5b67a701753009af5101979e06730a
capnbiggin <capnbiggin@users.noreply.github.com>
2025-03-02 18:21:28 -0600

    nav

---
+++ app/js/mobile-nav.js
@@ -0,0 +1,9 @@
+function toggleActive() {
+    const navToggle = document.querySelector(".header__nav-menu");
+    navToggle.classList.toggle("active")
+}
+
+function removeAtive() {
+    const navToggle = document.querySelector(".header__nav-menu");
+    navToggle.classList.remove("active")
+}
