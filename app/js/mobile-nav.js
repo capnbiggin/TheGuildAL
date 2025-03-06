dffc4e1d17c267b6f4800e4167a89a7de365c65b
capnbiggin <capnbiggin@users.noreply.github.com>
2025-03-05 20:18:29 -0600

    Dic Fixes

--- app/js/mobile-nav.js
+++ app/js/mobile-nav.js
@@ -1,15 +1,13 @@
-
-
 function addActive() {
-    const navToggle = document.querySelector(".topnav__menu");
-    const body = document.querySelector('body');
-    navToggle.classList.add("nav-active")
-    bodyScrollLockUpgrade.disableBodyScroll(body);
+	const navToggle = document.querySelector(".topnav__menu");
+	const body = document.querySelector("body");
+	navToggle.classList.add("nav-active");
+	bodyScrollLockUpgrade.disableBodyScroll(body);
 }
 
-function removeAtive() {
-    const navToggle = document.querySelector(".topnav__menu");
-    const body = document.querySelector('body');
-    navToggle.classList.remove("nav-active")
-    bodyScrollLockUpgrade.enableBodyScroll(body);
+function removeActive() {
+	const navToggle = document.querySelector(".topnav__menu");
+	const body = document.querySelector("body");
+	navToggle.classList.remove("nav-active");
+	bodyScrollLockUpgrade.enableBodyScroll(body);
 }
